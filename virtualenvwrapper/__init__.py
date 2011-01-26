fc05319a9e961bf39f969a8cf541bf39b8a08ccd
Doug Hellmann <doug.hellmann@gmail.com>
2011-01-26 07:03:34 -0500

    get the packaging right so the template shows up in the help output

---
+++ virtualenvwrapper/__init__.py
@@ -0,0 +1,4 @@
+"""virtualenvwrapper.django
+"""
+
+__import__('pkg_resources').declare_namespace(__name__)
---
+++ virtualenvwrapper/django_project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2011 Doug Hellmann.  All rights reserved.
+#
+"""Create Django projects automatically with virtualenvwrapper.
+"""
+
+import subprocess
+
+def template(args):
+    """Installs Django and runs django-admin to create a new project.
+    """
+    project = args[0]
+    subprocess.check_call(['pip', 'install', 'django'])
+    #subprocess.check_call(['pip', 'install', 'django'])
+    return
